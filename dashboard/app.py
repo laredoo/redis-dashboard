1e6e6e06f8ff30fd81d952b2ae7c77ae703365e2
Lucas Laredo <luckrocaabh@gmail.com>
2025-01-23 11:53:19 -0300

    local

--- dashboard/app.py
+++ dashboard/app.py
@@ -9,7 +9,7 @@ REDIS_HOST = os.environ['REDIS_HOST']
 REDIS_PORT = os.environ['REDIS_PORT']
 REDIS_INPUT_KEY = os.environ['REDIS_INPUT_KEY']
 
-config = {
+config_redis = {
     "timestamp": "2025-01-23 14:29:25.694864",
     "percent-memory-caching": 68.9197342004972, 
     "percent-network-egress": 32.298898723669225
@@ -17,11 +17,16 @@ config = {
 
 st.set_page_config(page_title="Hamlet Dashboardh: Resource Monitoring", layout="wide")
 
-st.header("Network Metrics")
-percent_network_egress = st.metric("Percent Network Egress", "0%")
+st.markdown("""
+    <style>
+        .stApp {
+            font-family: 'Georgia', sans-serif;
+            background-color: #000000;
+            color: white;
+        }
+    </style>
+""", unsafe_allow_html=True)
 
-st.header("Memory Metrics")
-percent_memory_cached = st.metric("Percent Memory Cached", "0%")
 
 class DashboardConfig:
     def __init__(self):
@@ -53,7 +58,7 @@ def get_redis_data(config: DashboardConfig):
     except Exception as e:
         st.error(f"Erro ao acessar o Redis: {e}")
         return None
-
+    
 def get_greeting(timestamp):
     hour = timestamp.hour
     if hour < 12:
@@ -69,36 +74,63 @@ def format_date(timestamp):
     suffix = {1: 'st', 2: 'nd', 3: 'rd'}.get(day % 10 if day not in [11, 12, 13] else 0, 'th')
     return dt.strftime(f"%B {day}{suffix}, %Y")
 
-def update_config(metrics):
+def update_config(metrics, percent_network_egress, percent_memory_cached):
     try:
         percent_network_egress.metric("Percent Network Egress", f"{metrics.get('percent-network-egress', 0):.2f}%")
         percent_memory_cached.metric("Percent Memory Cached", f"{metrics.get('percent-memory-caching', 0):.2f}%")
-
-        timestamp = metrics.get("timestamp")
-        if isinstance(timestamp, str):
-            timestamp = datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S.%f")
-
-
-        greeting = get_greeting(timestamp)
-
-        formatted_date = format_date(timestamp)
-
-        st.markdown(f"""
-            <div class="header-container">
-                <div class="greeting">{greeting}</div>
-                <div class="date">{formatted_date}</div>
-            </div>
-        """, unsafe_allow_html=True)
 
     except Exception as e:
         st.error(f"Error fetching data: {e}")
 
 def main():
 
-    config = DashboardConfig()
+    # config = DashboardConfig()
+    data = config_redis
+
+    string_timestamp = data.get("timestamp")
+    if isinstance(string_timestamp, str):
+        timestamp = datetime.strptime(string_timestamp, "%Y-%m-%d %H:%M:%S.%f")
+
+    greeting = get_greeting(timestamp)
+
+    formatted_date = format_date(string_timestamp)
+
+    st.markdown(f"""
+        <div class="header-container">
+            <h1 class="greeting">{greeting}</h1>
+            <h2 class="date">{formatted_date}</h2>
+        </div>
+    """, unsafe_allow_html=True)
+
+    st.markdown("""
+        <style>
+            .header-container{
+                font-family: 'Georgia', sans-serif;
+                text-align: center;
+                margin-bottom: 20px;
+            }
+            .greeting {
+                font-family: 'Georgia', sans-serif;
+                font-size: 32px;
+                font-weight: bold;
+                color: white;
+            }
+            .date {
+                font-family: 'Georgia', sans-serif;
+                font-size: 24px;
+                color: gray;
+            }
+        </style>
+    """, unsafe_allow_html=True)
+
+    st.header("Network Metrics")
+    percent_network_egress = st.metric("Percent Network Egress", "0%")
+
+    st.header("Memory Metrics")
+    percent_memory_cached = st.metric("Percent Memory Cached", "0%")
+
     while True:
-        data = get_redis_data(config)
-        update_config(data)
+        update_config(data, percent_network_egress, percent_memory_cached)
         time.sleep(5)
 
 if __name__ == "__main__":
